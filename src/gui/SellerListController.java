1643a31ee627f48fe7beef1ef787c784d639261f
Gian Antonio Batista <gian.antoniobatista@gmail.com>
2020-01-22 16:37:59 -0300

    Seller TableView

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,7 +1,7 @@
 package gui;
 
-import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -16,9 +16,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -26,8 +24,6 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.Pane;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Vendedor;
 import model.services.SellerService;
@@ -44,6 +40,15 @@ public class SellerListController implements Initializable, DataChangeListener {
 
 	@FXML
 	private TableColumn<Vendedor, String> tableColumnName;
+
+	@FXML
+	private TableColumn<Vendedor, String> tableColumnEmail;
+
+	@FXML
+	private TableColumn<Vendedor, Date> tableColumnBirthDate;
+
+	@FXML
+	private TableColumn<Vendedor, Double> tableColumnBaseSalary;
 
 	@FXML
 	private TableColumn<Vendedor, Vendedor> tableColumnEDIT;
@@ -78,7 +83,12 @@ public class SellerListController implements Initializable, DataChangeListener {
 		// Este comnado é um padrão do JavaFx para iniciar o comportamento das colunas
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("nome"));
-
+		tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+		tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("dataAniversario"));
+		Utils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyyy");
+		tableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("salario"));
+		Utils.formatTableColumnDouble(tableColumnBaseSalary, 2);
+		
 		// Este é um macete para fazer a tabela acompanhar o tamanho da tela
 		Stage stage = (Stage) Main.getMainScene().getWindow(); // Fazer um downCating
 		tableViewSeller.prefHeightProperty().bind(stage.heightProperty());
